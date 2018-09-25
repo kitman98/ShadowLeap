0b517bd19b67734d10fcbbfa0a12aeb763cc4242
kitman98 <42112353+kitman98@users.noreply.github.com>
2018-09-25 21:56:55 +1000

    Vehicles render properly
    To do list:
    ExtraLife
    Bulldozer push and squash
    Turtle submerged

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -5,8 +5,8 @@ public abstract class Vehicle extends Sprite {
 	private boolean moveRight;
 	
 	public final float getInitialX() {
-		return moveRight ? -World.TILE_SIZE / 2
-						 : App.SCREEN_WIDTH + World.TILE_SIZE / 2;
+		return moveRight ? -getImage().getWidth()/2
+						 : App.SCREEN_WIDTH + getImage().getWidth() / 2;
 	}
 	
 	public Vehicle(String imageSrc, float x, float y, boolean moveRight, String[] tags) {
@@ -15,18 +15,6 @@ public abstract class Vehicle extends Sprite {
 		this.moveRight = moveRight;
 	}
 
-/*
-	@Override
-	public void update(Input input, int delta) {
-		move(getSpeed() * delta * (moveRight ? 1 : -1), 0);
-		
-		// check if the vehicle has moved off the screen
-		if (getX() > App.SCREEN_WIDTH + getImage().getWidth() || getX() < -World.TILE_SIZE / 2
-		 || getY() > App.SCREEN_HEIGHT + World.TILE_SIZE / 2 || getY() < -World.TILE_SIZE / 2) {
-			setX(getInitialX() + getImage().getWidth()*(getMoveRight()? 1: -1));
-		}
-	}
-*/
 
     public void render() {
         if (hasTag(Sprite.SUBMERGES) && this instanceof Turtle) {
